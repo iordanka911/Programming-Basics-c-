7663e4277702dbb0d3c6d9f7b0ff579b7f11c302
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-01 23:38:49 +0300

    Create 02. Radians to Degrees.cs

---
+++ First Steps In Coding - Exercise/02. Radians to Degrees.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double degrees = radians * 180 / Math.PI;
+            Console.WriteLine($"{Math.Round(degrees)}");
+        }
+    }
+}
