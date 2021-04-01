9a23deffa8b62b5f048ed65979b353576336be52
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-01 23:31:28 +0300

    Create 05. Greeting by Name.cs

---
+++ First Steps In Coding - Lab/05. Greeting by Name.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            Console.Write("Hello, ");
+
+            Console.Write(name);
+
+            Console.WriteLine("!");
+        }
+    }
+}
