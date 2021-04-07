4d3afd7db5448b276958d7fdd14442d42933dae7
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-07 21:02:48 +0300

    Create 05. Password Guess..cs

---
+++ Conditional Statements - Lab/05. Password Guess..cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Number100200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            if (password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
