22b901ef5b5ab9e4f34f7d5b837e1b253f4716d3
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-08 07:43:12 +0300

    Create 04. Even Powers of 2.cs

---
+++ For Loop - Lab/04. Even Powers of 2.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Number1toN3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int power = 0; power <= n; power+=2)
+            {
+                {
+                    Console.WriteLine(Math.Pow(2, power));
+                }
+               
+            }
+        }
+    }
+
+
+}
