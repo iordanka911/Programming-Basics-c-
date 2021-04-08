53245d6a279a1ffb2d139a8934c076613ce0d89a
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-08 07:40:49 +0300

    Create 01. Numbers from 1 to 100.cs

---
+++ For Loop - Lab/01. Numbers from 1 to 100.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Number1to100
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        for (int i=1;i<=100;i++)
+            {
+                Console.WriteLine(i);
+            }                
+        }
+    }
+
+
+}
