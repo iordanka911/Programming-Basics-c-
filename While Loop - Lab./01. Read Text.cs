2386cd6770330de1469a56a6d6b4026cadce996f
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-07-02 10:11:48 +0300

    Create 01. Read Text.cs

---
+++ While Loop - Lab./01. Read Text.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ConsoleApp87
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            
+                while (command != "Stop")
+                {
+                    Console.WriteLine(command);
+                     command = Console.ReadLine();
+                   
+
+                }
+          
+          
+
+             
+        }
+    }
+}
