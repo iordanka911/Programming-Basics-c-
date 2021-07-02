e8307e9207229ddd770547a74264ada0c6471cfa
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-07-02 04:48:57 +0300

    Create 08.Cinema Ticket.cs

---
+++ Conditional Statements Advanced - Lab/08.Cinema Ticket.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string day = Console.ReadLine();
+            if (day=="Monday"||day=="Tuesday"||day=="Friday")
+            {
+                Console.WriteLine("12");
+            }
+            if (day == "Wednesday" || day == "Thursday")
+            {
+                Console.WriteLine("14");
+            }
+            if (day=="Saturday"||day=="Sunday")
+            {
+                Console.WriteLine("16");
+            }
+
+        }
+    }
+}
