c885fd95a019d93b8fcda77627bcf2ea0ce63577
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-07-02 04:48:14 +0300

    Create 07.Working Hours.cs

---
+++ Conditional Statements Advanced - Lab/07.Working Hours.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour=int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            if (day == "Sunday")
+            {
+                Console.WriteLine("closed");
+            }
+            else if (hour >= 10 && hour <= 18)
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+
+        }
+    }
+}
