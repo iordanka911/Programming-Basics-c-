b6eecd72094ec10026673816344268ca04fa39d2
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-07-02 21:46:53 +0300

    Create more exercisess.cs

---
+++ PB-More Exercises/More Exercises/more exercisess.cs
@@ -0,0 +1,150 @@
+https://app.sli.do/event/6emyihsj/live/questions
+https://softuni.bg/trainings/2996/computer-literacy-may-2020
+leetcode.com
+hackerrank
+https://www.hackerrank.com/
+StackOverflow
+Docs.microsoft.com
+Console.WriteLine(5+5);
+Console.WriteLine(“text”);
+Snippeti –cw –Console.writeline
+Zoomit- programata s koqto markirash ekrana
+/* text */ = 
+ Int x=5;
+X е името на променливата
+Типа е integer= цели числа
+Console.WriteLine(x);
+
+Int x=5;
+Int y=2;
+Console.WriteLine(x+y);
+int x=5; -> цели числа
+double y=2.2; - дробни числа
+Console.WriteLine(x+y);
+Типове данни текст:
+
+String name= “Petar”;
+Console.ReadLine();
+Parse- преобразува текста в число
+Ctrl+K+C – COMMENT CODE
+CTRL+K+U –INCOMMENT CODE
+$- ИНТЕРПОЛАЦИЯ
+
+For cicle 
+1 .Каква ще е стойността на променливата а след изпълнението на следната прогрма:
+int a=5;
+switch (a)
+{
+case 5:   - обединяване на два кейса
+case 6:
+ a=a+1;
+break;
+default:
+a=a+2;
+break;
+}
+Отговора е 6
+ 2. Какво ще се отпечата на конзолата, ако изпълним следната команда:
+Console.WriteLine(!(5==5)&&(4+1==5));
+FALSE
+int a = 5;
+            int b = 2;
+            double result = a / b;
+            Console.WriteLine($"{result}");
+            Отг.2
+
+https://www.youtube.com/watch?v=GhQdlIFylQ8
+/*Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your age:");
+            int age = int.Parse(Console.ReadLine());*/
+            int error = 0 / 0;
+            Console.WriteLine($"{error}");
+
+            int a = 5;
+            int b = 2;
+            Console.WriteLine(a%b);
+            Console.WriteLine(a/b);
+
+1. /const double courseOfDollars = 1.79549;
+            double usd = double.Parse(Console.ReadLine());
+            double convertBGN = usd * 1.79549;
+
+string name = Console.ReadLine();
+            Console.WriteLine($"Hello there, {name}");
+
+int n = int.Parse(Console.ReadLine());
+            for (int power = 0; power <= n; power++)
+            {
+                Console.WriteLine(Math.Pow(2,power));
+            }
+
+Отпечатване на степен
+int n = int.Parse(Console.ReadLine());
+            for (int power = 0; power <= n; power++)
+            {
+                if (power%2==0)
+                {
+                    Console.WriteLine(Math.Pow(2, power));
+                }
+int n = int.Parse(Console.ReadLine());
+            for (int power = 0; power <= n; power+=2)
+            {
+                {
+                    Console.WriteLine(Math.Pow(2, power));
+                }
+{
+,y yh g/’,mlp
+                    string text = "SoftUni";
+                    Console.WriteLine(text.Length);
+                }
+Property – свойство –дължина на текст
+	
+Console.WriteLine("Hello, {0}","Sanya");
+  string text = "SoftUni";
+  Console.WriteLine(text[4]);
+]
+{
+                    string text = "SoftUni";
+                    Console.WriteLine(text[text.Length-1]);
+                }
+               //1.text.Length
+               //All symbols in a text hava a 0- based index
+               //3. The last symbol in the text is text.Length-1
+string input = Console.ReadLine();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Console.WriteLine(input[i]);
+                }
+
+
+using System;
+
+namespace Number1toN3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x=1;x<=10;x++)
+            {
+                for (int y=1;y<=10;y++)
+                {
+                    int product = x * y;
+                    Console.WriteLine($"{x}*{y}={product}");
+                }
+            }
+            
+              
+                           
+        }
+    }
+
+
+}/
+,./
+
+
+http://www.asciitable.com/
+
